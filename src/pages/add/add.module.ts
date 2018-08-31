b6216da1e1d48f2ee965528c5b744766e287c44a
aveavemary <abbynisperos1212@gmail.com>
2018-08-31 19:37:27 +0800

    To Do

--- src/pages/add/add.module.ts
+++ src/pages/add/add.module.ts
@@ -1,13 +1,26 @@
-import { NgModule } from '@angular/core';
-import { IonicPageModule } from 'ionic-angular';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
 import { AddPage } from './add';
 
 @NgModule({
   declarations: [
+  	 MyApp,
+    HomePage,
     AddPage,
   ],
-  imports: [
-    IonicPageModule.forChild(AddPage),
+   imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
   ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    AddPage,
+  ],
+  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
-export class AddPageModule {}
+export class AppModule {}
